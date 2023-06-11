85e2c827e7dcb0a653b81d5e5bd4d6077482fb4a
Jonas <JNSAPH@icloud.com>
2023-06-11 06:04:08 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
